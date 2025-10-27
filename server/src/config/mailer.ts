48ccc75dc418eafdc16ea257851737b62b6890f3
tiendat3008 <trantiendat1679@gmail.com>
2025-10-27 18:06:45 +0700

    prettier

--- server/src/config/mailer.ts
+++ server/src/config/mailer.ts
@@ -1,10 +1,10 @@
+import { EMAIL_PASS, EMAIL_PORT, EMAIL_SENDER } from "@/constants/env";
 import nodemailer from "nodemailer";
-import { EMAIL_SENDER, EMAIL_PASS } from "@/constants/env";
 
 // Tạo transporter dùng Gmail
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
-  port: Number(process.env.EMAIL_PORT) || 465,
+  port: Number(EMAIL_PORT) || 465,
   secure: true, // Gmail cần SSL
   auth: {
     user: EMAIL_SENDER,
